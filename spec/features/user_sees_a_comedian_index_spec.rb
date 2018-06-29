309c94b698f2646958d98d86da492152b072a6c8
Chris Powell <chris.ian.powell@gmail.com>
2018-06-29 10:58:48 -0600

    adds user story four could not achieve user story 3

--- spec/features/user_sees_a_comedian_index_spec.rb
+++ spec/features/user_sees_a_comedian_index_spec.rb
@@ -16,5 +16,29 @@ RSpec.describe 'A visitor' do
       expect(page).to have_content('Mitch Hedberg')
       expect(page).to have_content('Hell Yes')
     end
+    it "sees a count of all specials for comedians" do
+      comic = Comedian.create(name: 'Mitch Hedberg', age: 48)
+      special = Special.create(name: 'Hell Yes', comedian_id: 1)
+      special = Special.create(name: 'Damn Right', comedian_id: 1)
+
+      visit '/comedians'
+
+      expect(page).to have_content('Mitch Hedberg')
+      expect(page).to have_content('Hell Yes')
+      expect(page).to have_content(2)
+    end
   end
+  # context 'visiting /comedians?age=46' do
+  #   it 'sees all comedians age 46' do
+  #     comic = Comedian.create(name: 'Mitch Hedberg', age: 48)
+  #     comic = Comedian.create(name: 'Tina Fey', age: 46)
+  #     comic = Comedian.create(name: 'Miss Chuckles', age: 46)
+  #
+  #     visit '/comedians?age=46'
+  #
+  #     expect(page).to have_content('Tina Fey')
+  #     expect(page).to have_content('Miss Chuckles')
+  #     expect(page).to have_content('Mitch Hedberg')
+  #   end
+  # end
 end
