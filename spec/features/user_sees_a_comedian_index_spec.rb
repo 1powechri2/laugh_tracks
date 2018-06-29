fc8c53e07bf06dfa144235bb09668e79c51492fe
Chris Powell <chris.ian.powell@gmail.com>
2018-06-29 10:42:00 -0600

    adds second user story

--- spec/features/user_sees_a_comedian_index_spec.rb
+++ spec/features/user_sees_a_comedian_index_spec.rb
@@ -7,5 +7,14 @@ RSpec.describe 'A visitor' do
 
       expect(page).to have_content('Mitch Hedberg')
     end
+    it "sees a comedian's specials" do
+      comic = Comedian.create(name: 'Mitch Hedberg', age: 48)
+      special = Special.create(name: 'Hell Yes', comedian_id: 1)
+
+      visit '/comedians'
+
+      expect(page).to have_content('Mitch Hedberg')
+      expect(page).to have_content('Hell Yes')
+    end
   end
 end
